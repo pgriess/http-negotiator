90371cf27f30410942a62f4400420136039d3afb
Peter Griess <peter.griess@oculus.com>
2018-09-15 01:26:35 -0500

    Support for wildcard matching
    - Change performNegotiation() to iterate over server values comparing
      them to the client's values and culling those that do not match.
      Previously we were iterating over client values. When we only
      supported exact matching there was no difference. Now, however, we
      need to validate the server's fixed, known set of options against
      what the client can handle which may include wildcards.
    - Add support for different matchers (do two values match? i.e. '*' can
      match anything) and comparators (sort the results of a matcher) to
      performNegotiation(). Use this to drive different behavior for
      Accept-Encoding vs. Accept.
    - Implement matchers and comparators for simple values, wildcard and
      media ranges.
    - Add a new awsNegotiateEncoding() high-level wrapper that does all of
      the heavy lifting of stringing functions together, injecting the
      'identity' header if necessary, etc. This logic is all specific to
      'Accept-Encoding'. It looks likely that 'Accept' will have its own
      special set of behavior, so splitting these up into separate drivers
      to encapsulate this logic looks useful.
    - So many tests.

--- http-content-negotiation.js
+++ http-content-negotiation.js
@@ -12,7 +12,7 @@
  *  - The header objects specified have values which conform to section 7 of RFC
  *    7230. For eample, Accept, Accept-Encoding support this. User-Agent does not.
  */
-exports.splitHeaders = function(headers) {
+const splitHeaders = function(headers) {
     return headers.map(function(ho) { return ho['value']; })
         .reduce(
             function(acc, val) {
@@ -20,6 +20,7 @@ exports.splitHeaders = function(headers) {
             },
             []);
 };
+exports.splitHeaders = splitHeaders;
 
 /*
  * Parse an HTTP header value with optional attributes, returning a tuple of
@@ -27,7 +28,7 @@ exports.splitHeaders = function(headers) {
  *
  * For example 'foo;a=1;b=2' would return ['foo', {'a': 1, 'b': 2}].
  */
-exports.parseHeaderValue = function(v) {
+const parseHeaderValue = function(v) {
     const s = v.split(';');
     if (s.length == 1) {
         return [v, {}];
@@ -45,6 +46,7 @@ exports.parseHeaderValue = function(v) {
 
     return [s[0], attrs];
 };
+exports.parseHeaderValue = parseHeaderValue;
 
 /*
  * Given an array of (value name, attribute dictionary) tuples, return a sorted
@@ -59,8 +61,8 @@ exports.parseHeaderValue = function(v) {
  *
  *      [['a', {'q': '5'}], ['a', {'q': '2'}], ['b', {'q': '3'}]]
  */
-exports.sortHeadersByQValue = function(headerValues) {
-    /* Parse q attributes, ensuring that all to 1 */
+const sortHeadersByQValue = function(headerValues) {
+    /* Parse q attributes, ensuring that all default to 1 */
     var headerValuesWithQValues = headerValues.map(function(vt) {
         var vn = vt[0];
         var va = vt[1];
@@ -87,6 +89,7 @@ exports.sortHeadersByQValue = function(headerValues) {
     /* Sort by values with highest 'q' attribute */
     return filteredValues.sort(function(a, b) { return b[1] - a[1]; });
 };
+exports.sortHeadersByQValue = sortHeadersByQValue;
 
 /*
  * Perform content negotiation.
@@ -94,22 +97,27 @@ exports.sortHeadersByQValue = function(headerValues) {
  * Given sorted arrays of supported (value name, q-value) tuples, select a
  * value that is mutuaully acceptable. Returns null is nothing could be found.
  */
-exports.performNegotiation = function(clientValues, serverValues) {
+const performNegotiation = function(clientValues, serverValues, matcher, comparator) {
     var scores = [];
-    for (var i = 0; i < clientValues.length; ++i) {
-        const cv = clientValues[i];
-        const sv = serverValues.find(function(sv) { return sv[0] === cv[0]; });
-        if (sv === undefined) {
-            continue;
+    serverValues.forEach(function(sv) {
+        /* Get all server values that match the given client value */
+        const matchingCv = clientValues.filter(function(cv) { return matcher(sv[0], cv[0]); });
+        if (matchingCv.length === 0) {
+            return;
         }
+
+        /* Pick the most specific server value for the current client value */
+        const cv = matchingCv
+            .sort(function(a, b) { return comparator(a[0], b[0]); })[0];
 
         const score = cv[1] * sv[1];
         if (score <= 0) {
-            continue;
+            return;
         }
 
-        scores.push([cv[0], score]);
-    }
+        /* Note that we push the server value here as it's expected not to be a wildcard */
+        scores.push([sv[0], score]);
+    });
 
     if (scores.length === 0) {
         return null;
@@ -117,3 +125,114 @@ exports.performNegotiation = function(clientValues, serverValues) {
 
     return scores.sort(function(a, b) { return b[1] - a[1]; })[0][0];
 };
+exports.performNegotiation = performNegotiation;
+
+/*
+ * Matcher and comparator for strict literals.
+ */
+const strictValueMatch = function(a, b) { return a === b; };
+exports.strictValueMatch = strictValueMatch;
+const strictValueCompare = function(a, b) { return 0; };
+exports.strictValueCompare = strictValueCompare;
+
+/*
+ * Matcher and comparator for wildcards.
+ * 
+ * For example, 'a' and '*' should match, but 'a' and 'b' should not. Wildcard
+ * matches should take lower precedence than exact matches.
+ */
+const wildcardValueMatch = function(a, b) {
+    if (a === '*' || b === '*') {
+        return true;
+    }
+
+    return strictValueMatch(a, b);
+};
+exports.wildcardValueMatch = wildcardValueMatch;
+const wildcardValueCompare = function(a, b) {
+    if (a === '*' || b === '*') {
+        if (a === '*' && b === '*') {
+            return 0;
+        } else if (a === '*') {
+            return 1;
+        } else {
+            return -1;
+        }
+    }
+
+    return strictValueCompare(a, b);
+};
+exports.wildcardValueCompare = wildcardValueCompare;
+
+/*
+ * Matcher and comparator for media ranges.
+ * 
+ * For example, 'text/plain' and 'text/*' should match, but 'text/plain' and
+ * 'text/javascript' should not. Wildcard matches should take lower precedence
+ * than exact matches.
+ */
+const mediaRangeValueMatch = function(a, b) {
+    const aTypes = a.split('/');
+    const bTypes = b.split('/');
+
+    return wildcardValueMatch(aTypes[0], bTypes[0]) &&
+        wildcardValueMatch(aTypes[1], bTypes[1]);
+};
+exports.mediaRangeValueMatch = mediaRangeValueMatch;
+const mediaRangeValueCompare = function(a, b) {
+    const aTypes = a.split('/');
+    const bTypes = b.split('/');
+
+    const c = wildcardValueCompare(aTypes[0], bTypes[0])
+    if (c !== 0) {
+        return c;
+    }
+
+    return wildcardValueCompare(aTypes[1], bTypes[1]);
+};
+exports.mediaRangeValueCompare = mediaRangeValueCompare;
+
+/*
+ * High-level wrapper to negotiate encoding.
+ */
+const awsNegotiateEncoding = function(headers, serverValues) {
+    /* 
+     * No Accept-Encoding header means the client will accept anything. Pick the
+     * highest-scoring server value and go with that.
+     */
+    if (!('accept-encoding' in headers)) {
+        return Array
+            .from(serverValues)
+            .sort(function(a, b) { return a[1] - b[1]; })
+            .pop()[0];
+    }
+
+    /* Parse values and attributes */
+    var parsedValues = splitHeaders(headers['accept-encoding'])
+        .map(parseHeaderValue);
+
+    /* 
+     * If no parsed values match 'identity' (i.e. it has not been overridden)
+     * add it as a default option per RFC 7231 section 5.3.4.
+     * 
+     * XXX: The RFC does not specify a default weight. We pick 1, but a case
+     *      could be made to pick something like 0.1 with the assumption that
+     *      anything else specified explicitly is likely to be more desirable than
+     *      this default.
+     * 
+     * XXX: Does this imply that we should attempt CN *without* this implied value
+     *      first and, only if there are no matches, then add it? That would make it
+     *      the absolute last option which feels like it's really the intent of the
+     *      RFC.
+     */
+    if (!parsedValues.some(function(v) { return wildcardValueMatch(v[0], 'identity'); })) {
+        parsedValues.unshift(['identity', {'q': '1'}]);
+    }
+
+    return performNegotiation(
+        sortHeadersByQValue(parsedValues),
+        serverValues,
+        wildcardValueMatch,
+        wildcardValueCompare);
+};
+exports.awsNegotiateEncoding = awsNegotiateEncoding;
--- test/test.js
+++ test/test.js
@@ -14,25 +14,29 @@ describe('splitHeaders()', function() {
             hcn.splitHeaders([
                 {'key': 'Accept-Encoding', 'value': 'deflate, gzip, br'},
                 {'key': 'Accept-Encoding', 'value': 'identity, deflate'}]),
-            ['deflate', 'gzip', 'br', 'identity', 'deflate']);
+            ['deflate', 'gzip', 'br', 'identity', 'deflate']
+        );
     });
     it('should strip optional whitespace', function() {
         assert.deepStrictEqual(
             hcn.splitHeaders([
                 {'key': 'Accept-Encoding', 'value': 'gzip , br,identity '}]),
-            ['gzip', 'br', 'identity']);
+            ['gzip', 'br', 'identity']
+        );
     });
     it('should not be confused by / characters', function() {
         assert.deepStrictEqual(
             hcn.splitHeaders([
                 {'key': 'Accept', 'value': 'image/png, image/webp'}]),
-            ['image/png', 'image/webp']);
+            ['image/png', 'image/webp']
+        );
     });
     it('should not be confused by * characters', function() {
         assert.deepStrictEqual(
             hcn.splitHeaders([
                 {'key': 'Accept', 'value': 'image/*, */*'}]),
-            ['image/*', '*/*']);
+            ['image/*', '*/*']
+        );
     });
 });
 
@@ -43,12 +47,14 @@ describe('parseHeaderValue()', function() {
     it('should parse multiple attributes with values', function() {
         assert.deepStrictEqual(
             hcn.parseHeaderValue('foo;a=1;b=2'),
-            ['foo', {'a': '1', 'b': '2'}]);
+            ['foo', {'a': '1', 'b': '2'}]
+        );
     });
     it('should handle attributes after wildcards', function() {
         assert.deepStrictEqual(
             hcn.parseHeaderValue('image/*;a=1;b=2'),
-            ['image/*', {'a': '1', 'b': '2'}]);
+            ['image/*', {'a': '1', 'b': '2'}]
+        );
     });
 });
 
@@ -56,12 +62,14 @@ describe('sortHeadersbyQValue', function() {
     it('should respect q attribute values', function() {
         assert.deepStrictEqual(
             hcn.sortHeadersByQValue([['gzip', {'q': '0.25'}]]),
-            [['gzip', 0.25]]);
+            [['gzip', 0.25]]
+        );
     });
     it('should infer a q attribute value of 1 if un-specified', function() {
         assert.deepStrictEqual(
             hcn.sortHeadersByQValue([['gzip', {}]]),
-            [['gzip', 1]]);
+            [['gzip', 1]]
+        );
     });
     it('should allow later items to override earlier ones', function() {
         assert.deepStrictEqual(
@@ -70,7 +78,8 @@ describe('sortHeadersbyQValue', function() {
                 ['br', {'q': '0.8'}],
                 ['gzip', {'q': '0.25'}],
             ]),
-            [['br', 0.8], ['gzip', 0.25]]);
+            [['br', 0.8], ['gzip', 0.25]]
+        );
     });
 });
 
@@ -79,7 +88,9 @@ describe('performNegotiation()', function() {
         assert.deepStrictEqual(
             hcn.performNegotiation(
                 [['a', 1], ['b', 1], ['c', 1]],
-                [['c', 1], ['z', 1]]
+                [['c', 1], ['z', 1]],
+                hcn.strictValueMatch,
+                hcn.strictValueCompare
             ),
             'c'
         );
@@ -88,7 +99,9 @@ describe('performNegotiation()', function() {
         assert.deepStrictEqual(
             hcn.performNegotiation(
                 [['a', 1], ['b', 1], ['c', 1]],
-                [['z', 1]]
+                [['z', 1]],
+                hcn.strictValueMatch,
+                hcn.strictValueCompare
             ),
             null
         );
@@ -97,7 +110,9 @@ describe('performNegotiation()', function() {
         assert.deepStrictEqual(
             hcn.performNegotiation(
                 [['a', 1], ['b', 1], ['c', 0.8]],
-                [['b', 0.9], ['c', 1]]
+                [['b', 0.9], ['c', 1]],
+                hcn.strictValueMatch,
+                hcn.strictValueCompare
             ),
             'b'
         );
@@ -106,7 +121,9 @@ describe('performNegotiation()', function() {
         assert.deepStrictEqual(
             hcn.performNegotiation(
                 [['a', 1], ['b', 1], ['c', 1]],
-                [['b', 0.9], ['c', 1]]
+                [['b', 0.9], ['c', 1]],
+                hcn.strictValueMatch,
+                hcn.strictValueCompare
             ),
             'c'
         );
@@ -115,8 +132,154 @@ describe('performNegotiation()', function() {
         assert.deepStrictEqual(
             hcn.performNegotiation(
                 [['a', 1]],
-                [['a', 0]]),
+                [['a', 0]],
+                hcn.strictValueMatch,
+                hcn.strictValueCompare
+            ),
             null
+        );
+    });
+    it('should support wildcard matching', function() {
+        assert.deepStrictEqual(
+            hcn.performNegotiation(
+                [['a', 1], ['*', 0.5]],
+                [['a', 0.25], ['b', 1]],
+                hcn.wildcardValueMatch,
+                hcn.wildcardValueCompare
+            ),
+            'b'
+        );
+    });
+    it('should not apply wildcard to earlier values', function() {
+        assert.deepStrictEqual(
+            hcn.performNegotiation(
+                [['a', 1], ['*', 0.5]],
+                [['a', 0.8], ['b', 1]],
+                hcn.wildcardValueMatch,
+                hcn.wildcardValueCompare
+            ),
+            'a'
+        );
+    });
+});
+
+describe('matchersAndComparators', function() {
+    describe('strict', function() {
+        it('should match on identical values', function() {
+            assert.equal(hcn.strictValueMatch('a', 'a'), true);
+        });
+        it('should not match on different values', function() {
+            assert.equal(hcn.strictValueMatch('a', 'b'), false);
+        });
+        it('should not match on identical prefixes', function() {
+            assert.equal(hcn.strictValueMatch('ace', 'aceq'), false);
+        });
+        it('should compare all values to 0', function() {
+            assert.equal(hcn.strictValueCompare('a', 'a'), 0);
+            assert.equal(hcn.strictValueCompare('a', 'b'), 0);
+        });
+    });
+    describe('wildcard', function() {
+        it('should match on identical values', function() {
+            assert.equal(hcn.wildcardValueMatch('a', 'a'), true);
+        });
+        it('should not match on different values', function() {
+            assert.equal(hcn.wildcardValueMatch('a', 'b'), false);
+        });
+        it('should match on wildcards', function() {
+            assert.equal(hcn.wildcardValueMatch('a', '*'), true);
+            assert.equal(hcn.wildcardValueMatch('*', 'b'), true);
+            assert.equal(hcn.wildcardValueMatch('*', '*'), true);
+        });
+        it('should compare wildcards after exact matches', function() {
+            assert.equal(hcn.wildcardValueCompare('a', '*'), -1);
+            assert.equal(hcn.wildcardValueCompare('*', 'a'), 1);
+        });
+        it('should compare identical values to 0', function() {
+            assert.equal(hcn.wildcardValueCompare('a', 'a'), 0);
+            assert.equal(hcn.wildcardValueCompare('*', '*'), 0);
+        });
+    });
+    describe('mediaRange', function() {
+        it('should match on identical values', function() {
+            assert.equal(hcn.mediaRangeValueMatch('a/aa', 'a/aa'), true);
+        });
+        it('should not match on different types', function() {
+            assert.equal(hcn.mediaRangeValueMatch('a/aa', 'b/aa'), false);
+        });
+        it('should not match on different subtypes', function() {
+            assert.equal(hcn.mediaRangeValueMatch('a/aa', 'a/bb'), false);
+        });
+        it('should match on types', function() {
+            assert.equal(hcn.mediaRangeValueMatch('a/aa', '*/aa'), true);
+            assert.equal(hcn.mediaRangeValueMatch('a/aa', '*/*'), true);
+            assert.equal(hcn.mediaRangeValueMatch('*/aa', 'a/aa'), true);
+            assert.equal(hcn.mediaRangeValueMatch('*/*', 'a/aa'), true);
+        });
+        it('should match on subtypes', function() {
+            assert.equal(hcn.mediaRangeValueMatch('a/aa', 'a/*'), true);
+            assert.equal(hcn.mediaRangeValueMatch('a/aa', '*/*'), true);
+            assert.equal(hcn.mediaRangeValueMatch('a/*', 'a/aa'), true);
+            assert.equal(hcn.mediaRangeValueMatch('*/*', 'a/aa'), true);
+        });
+        it('should compare wildcards after exact matches', function() {
+            assert.equal(hcn.mediaRangeValueCompare('a/aa', 'a/aa'), 0);
+            assert.equal(hcn.mediaRangeValueCompare('a/aa', 'a/*'), -1);
+            assert.equal(hcn.mediaRangeValueCompare('a/aa', '*/aa'), -1);
+            assert.equal(hcn.mediaRangeValueCompare('a/aa', '*/*'), -1);
+
+            assert.equal(hcn.mediaRangeValueCompare('a/*', 'a/aa'), 1);
+            assert.equal(hcn.mediaRangeValueCompare('a/*', 'a/*'), 0);
+            assert.equal(hcn.mediaRangeValueCompare('a/*', '*/aa'), -1);
+            assert.equal(hcn.mediaRangeValueCompare('a/*', '*/*'), -1);
+
+            assert.equal(hcn.mediaRangeValueCompare('*/aa', 'a/aa'), 1);
+            assert.equal(hcn.mediaRangeValueCompare('*/aa', 'a/*'), 1);
+            assert.equal(hcn.mediaRangeValueCompare('*/aa', '*/aa'), 0);
+            assert.equal(hcn.mediaRangeValueCompare('*/aa', '*/*'), -1);
+
+            assert.equal(hcn.mediaRangeValueCompare('*/*', 'a/aa'), 1);
+            assert.equal(hcn.mediaRangeValueCompare('*/*', 'a/*'), 1);
+            assert.equal(hcn.mediaRangeValueCompare('*/*', '*/aa'), 1);
+            assert.equal(hcn.mediaRangeValueCompare('*/*', '*/*'), 0);
+        });
+    });
+});
+
+describe('awsNegotiateEncoding', function() {
+    it('should return the best server value if no Accept-Encoding found', function() {
+        assert.equal(
+            hcn.awsNegotiateEncoding(
+                {'user-agent': [{'key': 'User-Agent', 'value': 'Firefox'}]},
+                [['gzip', 0.5], ['br', 1], ['identity', 0.1]]),
+            'br'
+        );
+    });
+    it('should assume an implicit identity;q=1 value', function() {
+        assert.equal(
+            hcn.awsNegotiateEncoding(
+                {'accept-encoding':
+                    [{'key': 'Accept-Encoding', 'value': 'gzip;q=0.5'}]},
+                [['identity', 1], ['gzip', 1]]),
+            'identity'
+        );
+    });
+    it('should allow implicit identity value to be overridden explicitly', function() {
+        assert.equal(
+            hcn.awsNegotiateEncoding(
+                {'accept-encoding':
+                    [{'key': 'Accept-Encoding', 'value': 'gzip;q=0.5, identity;q=0'}]},
+                [['identity', 1], ['gzip', 1]]),
+            'gzip'
+        );
+    });
+    it('should allow implicit identity value to be overridden by wildcard', function() {
+        assert.equal(
+            hcn.awsNegotiateEncoding(
+                {'accept-encoding':
+                    [{'key': 'Accept-Encoding', 'value': 'gzip;q=0.5, *;q=0'}]},
+                [['identity', 1], ['gzip', 1]]),
+            'gzip'
         );
     });
 });
